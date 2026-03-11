5d55d33732d3e9a82248b6ee8531b2001df4a486
Andreas Cormack <andreas@passthekeys.co.uk>
2026-03-11 20:43:28 +0000

    fix asgi error

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -3,4 +3,17 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
 
-application = get_asgi_application()
+django_application = get_asgi_application()
+
+
+async def application(scope, receive, send):
+    if scope["type"] == "lifespan":
+        while True:
+            message = await receive()
+            if message["type"] == "lifespan.startup":
+                await send({"type": "lifespan.startup.complete"})
+            elif message["type"] == "lifespan.shutdown":
+                await send({"type": "lifespan.shutdown.complete"})
+                return
+    else:
+        await django_application(scope, receive, send)
