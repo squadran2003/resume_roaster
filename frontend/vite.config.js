586429a544fd4c832be0b9b91f3c28ba23462ace
Andreas Cormack <andreas@passthekeys.co.uk>
2026-03-17 21:38:34 +0000

    set index credit message to env value

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,14 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   plugins: [
     vue(),
     vuetify({ autoImport: true }),
