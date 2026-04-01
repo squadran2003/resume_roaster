b4bc644afb705e56b7b729159658f8e750fc4112
Andreas Cormack <andreas@passthekeys.co.uk>
2026-04-01 21:41:19 +0100

    fix python logger issue

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -86,15 +86,14 @@ LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "formatters": {
-        "json": {
-            "()": "pythonjsonlogger.jsonlogger.JsonFormatter",
+        "verbose": {
             "format": "%(asctime)s %(name)s %(levelname)s %(message)s",
         },
     },
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
-            "formatter": "json",
+            "formatter": "verbose",
         },
         "sentry": {
             "level": "ERROR",
